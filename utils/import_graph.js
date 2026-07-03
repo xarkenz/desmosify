e73188e83314bcb23690b11d147aa2bf2b9309ef
xarkenz <seanedwardsclarke@gmail.com>
2026-07-03 00:01:15 -0500

    added more information to minesweeper

--- utils/import_graph.js
+++ utils/import_graph.js
@@ -22,4 +22,4 @@
     i.click();
 })();
 
-// (()=>{let i=document.createElement("input");i.type="file";i.accept="application/json";i.addEventListener("change",()=>{if(i.files){let f=new FileReader();f.onerror=alert;f.onload=()=>{try{Calc.setState(JSON.parse(f.result));Calc.controller._hasUnsavedChanges=true}catch(e){alert(e)}};f.readAsText(i.files[0])}});i.click()})();
+// (()=>{let i=document.createElement("input");i.type="file";i.accept="application/json";i.addEventListener("change",()=>{if(i.files){let f=new FileReader();f.onerror=alert;f.onload=()=>{try{Calc.setState(JSON.parse(f.result),{allowUndo:true});Calc.controller._hasUnsavedChanges=true}catch(e){alert(e)}};f.readAsText(i.files[0])}});i.click()})();
