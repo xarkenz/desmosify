83e9ead2b3d438340256bb8262bb1c8e27e617e2
xarkenz <seanedwardsclarke@gmail.com>
2026-08-26 00:40:43 -0500

    implemented rudimentary compiler hook

--- desmosify-wasm/src/lib.rs
+++ desmosify-wasm/src/lib.rs
@@ -1,7 +1,18 @@
+use std::path::Path;
 use wasm_bindgen::prelude::*;
+use desmosify::{CompileOptions, SourceFile, SourceFiles};
 
 #[wasm_bindgen]
 pub fn compile(source: &str) -> Result<String, JsValue> {
     console_error_panic_hook::set_once();
-    Ok(source.to_string())
+
+    let sources = SourceFiles::from_iter(std::iter::once(SourceFile {
+        path: Path::new("<playground>"),
+        content: source,
+    }));
+
+    desmosify::compile(&sources, &CompileOptions::default_for_target("desmos-graphing"))
+        .map_err(|error| {
+            JsValue::from_str(&format!("Error: {}", error.display_with_context(&sources)))
+        })
 }
